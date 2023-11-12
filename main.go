1f00532602989e3101f045b9f2d52e7834ea6f70
Hugh McKenna <hfmckenna@outlook.com>
2023-11-12 14:38:13 +0000

    refactor: Use given port

--- main.go
+++ main.go
@@ -4,17 +4,16 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	home := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 		tmpl.Execute(w, nil)
 	}
 	http.HandleFunc("/", home)
 
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
