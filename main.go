a8f102105c009268bc8202fa3581818eb02b8b34
Hugh McKenna <hfmckenna@outlook.com>
2023-11-12 18:20:31 +0000

    fix: Correct port for EC2

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		// Apple listens on 5000, AWS needs 5000
+		if runtime.GOOS == "darwin" {
+			port = "8080"
+		} else {
+			port = "5000"
+		}
 	}
+
+	f, _ := os.Create("/var/log/golang/golang-server.log")
+	defer f.Close()
+	log.SetOutput(f)
 
 	const indexPage = "templates/index.html"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, indexPage)
+		tmpl := template.Must(template.ParseFiles(indexPage))
+		tmpl.Execute(w, nil)
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
