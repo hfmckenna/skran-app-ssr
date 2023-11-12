97901fd429b6f024308dddda919e7935b82c4e4e
Hugh McKenna <hfmckenna@outlook.com>
2023-11-12 18:11:04 +0000

    feat: Default Go

--- main.go
+++ main.go
@@ -9,18 +9,14 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "5000"
 	}
 
-	f, _ := os.Create("/var/log/golang/golang-server.log")
-	defer f.Close()
-	log.SetOutput(f)
-
-	const indexPage = "index.html"
+	const indexPage = "templates/index.html"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, indexPage)
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	http.ListenAndServe(":"+port, nil)
 }
