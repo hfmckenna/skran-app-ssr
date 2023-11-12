3b38d313cb12370b876beb118e5cf32616035cea
Hugh McKenna <hfmckenna@outlook.com>
2023-11-12 22:11:55 +0000

    feat: Add DynamoDB

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
 )
+
+// Create struct to hold info about new item
+type Item struct {
+	pk   string
+	sk   string
+	test string
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -23,10 +35,46 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
 	const indexPage = "templates/index.html"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles(indexPage))
-		tmpl.Execute(w, nil)
+
+		result, err := svc.GetItem(&dynamodb.GetItemInput{
+			TableName: aws.String("skran-app"),
+			Key: map[string]*dynamodb.AttributeValue{
+				"pk": {
+					N: aws.String("test"),
+				},
+				"sk": {
+					S: aws.String("test"),
+				},
+			},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if result.Item == nil {
+			return
+		}
+
+		item := Item{}
+
+		err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		}
+		tmpl.Execute(w, item)
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
