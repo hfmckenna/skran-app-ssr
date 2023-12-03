0428cacd500016b5918ea9ef578ba92d76851c9e
Hugh McKenna <hfmckenna@outlook.com>
2023-12-03 14:37:17 +0000

    feat: Static files

--- cdk.go
+++ cdk.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	acm "github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
+	cloudfront "github.com/aws/aws-cdk-go/awscdk/v2/awscloudfront"
+	origins "github.com/aws/aws-cdk-go/awscdk/v2/awscloudfrontorigins"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
-	"github.com/aws/aws-cdk-go/awscdkapigatewayv2alpha/v2"
-	"github.com/aws/aws-cdk-go/awscdkapigatewayv2integrationsalpha/v2"
-	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
+	route53 "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
+	route53targets "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53targets"
+	s3 "github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	s3assets "github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
+	s3deploy "github.com/aws/aws-cdk-go/awscdk/v2/awss3deployment"
+	apiGateway "github.com/aws/aws-cdk-go/awscdkapigatewayv2alpha/v2"
+	apiGatewayIntegrations "github.com/aws/aws-cdk-go/awscdkapigatewayv2integrationsalpha/v2"
+	lambda "github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"os"
+	"strings"
 
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -15,6 +25,12 @@ import (
 
 type SkranAppSsrStackProps struct {
 	awscdk.StackProps
+	stackDetails StackConfigs
+}
+
+type StackConfigs struct {
+	HostedZoneName string `field:"optional"`
+	Subdomain      string `field:"optional"`
 }
 
 func SkranAppSsrStack(scope constructs.Construct, id string, props *SkranAppSsrStackProps) awscdk.Stack {
@@ -24,18 +40,132 @@ func SkranAppSsrStack(scope constructs.Construct, id string, props *SkranAppSsrS
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	homeHandler := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("skran-ssr-home"), &awscdklambdagoalpha.GoFunctionProps{
+	var cloudfrontDistribution cloudfront.Distribution
+	hostedZoneName := props.stackDetails.HostedZoneName
+	subdomain := props.stackDetails.Subdomain
+
+	// Creates Origin Access Identity (OAI) to only allow CloudFront to get content
+	cloudfrontOAI := cloudfront.NewOriginAccessIdentity(stack, jsii.String("CloudFrontOAI"), &cloudfront.OriginAccessIdentityProps{})
+
+	// Creates S3 Bucket to store our static site content
+	siteBucket := s3.NewBucket(stack, jsii.String("StaticSiteBucket"), &s3.BucketProps{
+		BlockPublicAccess: s3.BlockPublicAccess_BLOCK_ALL(),
+		PublicReadAccess:  jsii.Bool(false),
+		Versioned:         jsii.Bool(true),
+	})
+
+	// Adds a policy to the S3 Bucket that allows the OAI to get objects
+	siteBucket.GrantRead(cloudfrontOAI, nil)
+
+	cloudfrontDefaultBehavior := &cloudfront.BehaviorOptions{
+		// Sets the S3 Bucket as the origin and tells CloudFront to use the created OAI to access it
+		Origin: origins.NewS3Origin(siteBucket, &origins.S3OriginProps{
+			OriginId:             jsii.String("CloudFrontS3Access"),
+			OriginAccessIdentity: cloudfrontOAI,
+		}),
+		ViewerProtocolPolicy: cloudfront.ViewerProtocolPolicy_REDIRECT_TO_HTTPS,
+	}
+
+	cloudfrontErrorResponses := &[]*cloudfront.ErrorResponse{
+		{
+			HttpStatus:         jsii.Number(403),
+			ResponseHttpStatus: jsii.Number(403),
+			ResponsePagePath:   jsii.String("/error.html"),
+		},
+	}
+
+	// If Route 53 Hosted Zone is set, update AWS Certificate Manager, Route 53, and CloudFront accordingly
+	if strings.TrimSpace(hostedZoneName) != "" {
+		var fullDomain string
+
+		// If a subdomain is not set
+		if strings.TrimSpace(subdomain) == "" {
+			fullDomain = hostedZoneName
+		} else {
+			fullDomain = fmt.Sprintf("%s.%s", subdomain, hostedZoneName)
+		}
+
+		// Searches Route 53 for existing zone using hosted zone name
+		hostedZone := route53.HostedZone_FromLookup(stack, jsii.String("MyHostedZone"), &route53.HostedZoneProviderProps{
+			DomainName:  jsii.String(hostedZoneName),
+			PrivateZone: jsii.Bool(false),
+		})
+
+		awscdk.Annotations_Of(hostedZone).AddInfo(jsii.String("Route 53 Hosted Zone is set"))
+
+		// Creates an SSL/TLS certificate
+		certificate := acm.NewCertificate(stack, jsii.String("StaticSiteCert"), &acm.CertificateProps{
+			DomainName: jsii.String(fullDomain),
+			Validation: acm.CertificateValidation_FromDns(hostedZone),
+		})
+
+		// Creates a new CloudFront Distribution with a custom Route 53 domain and custom SSL/TLS Certificate
+		cloudfrontDistribution = cloudfront.NewDistribution(stack, jsii.String("SiteDistribution"), &cloudfront.DistributionProps{
+			DefaultRootObject: jsii.String("index.html"),
+			DefaultBehavior:   cloudfrontDefaultBehavior,
+			ErrorResponses:    cloudfrontErrorResponses,
+			Certificate:       certificate,
+			DomainNames: &[]*string{
+				jsii.String(fullDomain),
+			},
+		})
+
+		// Creates Route 53 record to point to the CloudFront Distribution
+		publicEndpoint := route53.NewARecord(stack, jsii.String("Route53ToCloudFront"), &route53.ARecordProps{
+			Zone:       hostedZone,
+			RecordName: jsii.String(subdomain),
+			Target:     route53.RecordTarget_FromAlias(route53targets.NewCloudFrontTarget(cloudfrontDistribution)),
+		})
+
+		// Outputs public Route 53 endpoint
+		awscdk.NewCfnOutput(stack, jsii.String("Route53Endpoint"), &awscdk.CfnOutputProps{
+			Value: publicEndpoint.DomainName(),
+		})
+	} else {
+		awscdk.Annotations_Of(stack).AddInfo(jsii.String("Route 53 Hosted Zone is NOT set"))
+
+		// Creates a new CloudFront Distribution
+		cloudfrontDistribution = cloudfront.NewDistribution(stack, jsii.String("SiteDistribution"), &cloudfront.DistributionProps{
+			DefaultRootObject: jsii.String("index.html"),
+			DefaultBehavior:   cloudfrontDefaultBehavior,
+			ErrorResponses:    cloudfrontErrorResponses,
+		})
+	}
+
+	// Copies site assets from a local path to the S3 Bucket
+	s3deploy.NewBucketDeployment(stack, jsii.String("S3ContentDeployment"), &s3deploy.BucketDeploymentProps{
+		DestinationBucket: siteBucket,
+		Sources: &[]s3deploy.ISource{
+			s3deploy.Source_Asset(jsii.String("./public"), &s3assets.AssetOptions{}),
+		},
+		Distribution: cloudfrontDistribution,
+		DistributionPaths: &[]*string{
+			jsii.String("/*"),
+		},
+	})
+
+	// Outputs CloudFront endpoint
+	awscdk.NewCfnOutput(stack, jsii.String("CloudFrontEndpoint"), &awscdk.CfnOutputProps{
+		Value: cloudfrontDistribution.DistributionDomainName(),
+	})
+
+	// Outputs S3 Bucket endpoint (to show that it's not public)
+	awscdk.NewCfnOutput(stack, jsii.String("S3BucketEndpoint"), &awscdk.CfnOutputProps{
+		Value: siteBucket.BucketDomainName(),
+	})
+
+	homeHandler := lambda.NewGoFunction(stack, jsii.String("skran-app-ssr-home"), &lambda.GoFunctionProps{
 		FunctionName: jsii.String("skran-app-ssr-home"),
 		Runtime:      awslambda.Runtime_GO_1_X(),
 		Entry:        jsii.String("./src"),
-		Bundling: &awscdklambdagoalpha.BundlingOptions{
+		Bundling: &lambda.BundlingOptions{
 			GoBuildFlags: jsii.Strings(`-ldflags "-s -w"`),
 		},
 	})
 
-	homeHttp := awscdkapigatewayv2alpha.NewHttpApi(stack, jsii.String("home"), &awscdkapigatewayv2alpha.HttpApiProps{ApiName: jsii.String("home")})
+	homeHttp := apiGateway.NewHttpApi(stack, jsii.String("skran-app-ssr-root"), &apiGateway.HttpApiProps{ApiName: jsii.String("root")})
 
-	homeHttp.AddRoutes(&awscdkapigatewayv2alpha.AddRoutesOptions{Path: jsii.String("/"), Methods: &[]awscdkapigatewayv2alpha.HttpMethod{awscdkapigatewayv2alpha.HttpMethod_GET}, Integration: awscdkapigatewayv2integrationsalpha.NewHttpLambdaIntegration(jsii.String("HomeLambdaIntegration"), homeHandler, &awscdkapigatewayv2integrationsalpha.HttpLambdaIntegrationProps{})})
+	homeHttp.AddRoutes(&apiGateway.AddRoutesOptions{Path: jsii.String("/"), Methods: &[]apiGateway.HttpMethod{apiGateway.HttpMethod_GET}, Integration: apiGatewayIntegrations.NewHttpLambdaIntegration(jsii.String("RootLambdaIntegration"), homeHandler, &apiGatewayIntegrations.HttpLambdaIntegrationProps{})})
 
 	return stack
 }
@@ -48,6 +178,15 @@ func main() {
 	SkranAppSsrStack(app, "SkranAppSsrStack", &SkranAppSsrStackProps{
 		awscdk.StackProps{
 			Env: env(),
+		},
+		StackConfigs{
+			// Optional
+			// Set to an existing public Route 53 Hosted Zone in your control e.g. "amazon.com". Otherwise, set to ""
+			HostedZoneName: "skran.app",
+
+			// Optional
+			// Add a subdomain to the hosted zone e.g. "aws". Otherwise, set to ""
+			Subdomain: "assets",
 		},
 	})
 
