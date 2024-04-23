203273739b67b6f9b1b35fb46291599d1f5c870a
Hugh McKenna <hfmckenna@outlook.com>
2024-04-23 22:21:12 +0100

    feat: Add HTMX

--- api/main.go
+++ api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html"}}, nil
+	query := req.QueryStringParameters["q"]
+	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html"}, Body: fmt.Sprintf("<span>%s</span>", query)}, nil
 }
 
 func main() {
