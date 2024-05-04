7563451d2e26a41caad89a087b1043ce16959fe5
Hugh McKenna <hfmckenna@outlook.com>
2024-05-04 17:22:25 +0100

    feat: Add find

--- api/main.go
+++ api/main.go
@@ -24,10 +24,28 @@ func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	find := upperSnakeCase(req.QueryStringParameters["find"]) + "#"
 	response := ""
 	if len(query) > 2 && len(query) < 20 {
-		response = queryDynamo(query)
+		result, err := queryDynamo(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+		uniqueItems := dedupeSearch(result)
+		html := make([]string, len(uniqueItems))
+		for i, item := range uniqueItems {
+			html[i] = fmt.Sprintf("<button hx-get=\"/v1/search\" name=\"find\" hx-target=\"#search-results\" value=\"%s\">%s</button>", item.Title, item.Title)
+		}
+		response = strings.Join(html, "\n")
 	}
 	if len(find) > 4 {
-		response = queryDynamo(find)
+		result, err := queryDynamo(find)
+		if err != nil {
+			log.Fatal(err)
+		}
+		uniqueItems := dedupeSearch(result)
+		html := make([]string, len(uniqueItems))
+		for i, item := range uniqueItems {
+			html[i] = fmt.Sprintf("<div><a href=\"/recipes/%s\">%s</a></div>", item.RecipeTitle, item.RecipeId)
+		}
+		response = strings.Join(html, "\n")
 	}
 	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{"Content-Type": "text/html"}, Body: response}, nil
 }
@@ -52,8 +70,8 @@ func upperSnakeCase(s string) string {
 	return snake
 }
 
-func queryDynamo(query string) string {
-	response := ""
+func queryDynamo(query string) ([]models.SearchItem, error) {
+	var items []models.SearchItem
 	result, err := ddb.Query(&dynamodb.QueryInput{
 		TableName:              aws.String("SkranAppTable"),
 		KeyConditionExpression: jsii.String("#pk = :char and begins_with(#sk, :query)"),
@@ -67,35 +85,34 @@ func queryDynamo(query string) string {
 		},
 	})
 	if result.Items != nil {
-		html := make([]string, len(result.Items))
-		for i, item := range result.Items {
+		for _, item := range result.Items {
 			searchItem := models.SearchItem{}
 			err = dynamodbattribute.UnmarshalMap(item, &searchItem)
-			if err != nil {
-				panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-			}
-			html[i] = fmt.Sprintf("<button hx-get=\"/v1/search\" name=\"find\" hx-target=\"#search-results\" value=\"%s\">%s</button>", searchItem.Title, searchItem.Title)
 		}
-		response = strings.Join(dedupeStrings(html), "")
-	} else if err != nil {
-		log.Fatal(err)
 	}
-	return response
+	return items, err
 }
 
-func dedupeStrings(input []string) []string {
-	// Create a map to track unique strings.
-	seen := make(map[string]struct{})
-	// Create a slice to store the result.
-	var result []string
-	// Loop over each string in the input slice.
-	for _, str := range input {
-		// Check if the string is already in the map.
-		if _, exists := seen[str]; !exists {
-			// If the string is not in the map, add it to the result slice and mark it as seen.
-			result = append(result, str)
-			seen[str] = struct{}{}
+func dedupeRecipes(items []models.SearchItem) []models.SearchItem {
+	var deduped []models.SearchItem
+	for _, item := range items {
+		seen := make(map[string]string)
+		if _, exists := seen[item.RecipeId]; !exists {
+			deduped = append(deduped, item)
+			seen[item.RecipeId] = item.RecipeId
 		}
 	}
-	return result
+	return deduped
+}
+
+func dedupeSearch(items []models.SearchItem) []models.SearchItem {
+	var deduped []models.SearchItem
+	for _, item := range items {
+		seen := make(map[string]string)
+		if _, exists := seen[item.Title]; !exists {
+			deduped = append(deduped, item)
+			seen[item.Title] = item.Title
+		}
+	}
+	return deduped
 }
